ae167a81f19ed27f515b535a4b4a6f9d15a6fdaf
wesleysouza <wsouzacs@gmail.com>
2022-02-06 16:08:37 -0300

    updates

---
+++ comp0212/code/aula-01/01-checa-numero.py
@@ -0,0 +1,26 @@
+import time
+
+def ler_arquivo():
+    arquivo = open("numeros.txt", "r")
+    dados = arquivo.readlines()
+    arquivo.close()
+    return dados
+
+def conta_ocorrencias(lista, numero):
+    contador = 0
+    for i in lista:
+        if int(i) == numero:
+            contador = contador + 1
+    return contador
+
+import time
+
+inicio = time.time()
+
+dados = ler_arquivo()
+contador = conta_ocorrencias(dados, 10)
+print("Numero de ocorrencias: " + str(contador))
+
+fim = time.time()
+
+print("Tempo total:" + str(fim - inicio))
---
+++ comp0212/code/aula-01/01-gerador-python.py
@@ -0,0 +1,13 @@
+from random import randint
+
+arquivo = open("numeros.txt", "w")
+
+qtNumeros = 10000000
+limiteSuperior = 100
+contador = 0
+
+while (contador < qtNumeros):
+    arquivo.write(str(randint(0, limiteSuperior)) + "\n")
+    contador = contador + 1
+
+arquivo.close()
